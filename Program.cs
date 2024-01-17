1a1068afae823cc6bd0bfce607b0dbea3bca98ed
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-17 09:15:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GA_Inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inheritance
+
+
+
+
+        } // main
+
+    } // class
+     
+} // namespace
